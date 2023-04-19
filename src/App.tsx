9529ae1184f08e867fa4789c20521b4fde254003
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-04-19 14:23:24 +0200

    fixed footer going over text boxes

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App() {
       <header className="p-4 items-center justify-center w-screen bg-sky-500">
         <h1 className="text-2xl">Speech recognition</h1>
       </header>
-      <div id="speechRecognitionOutput" className="w-screen relative">
+      <div id="speechRecognitionOutput" className="w-screen relative mb-24">
         <p className="sentenceToRead">De kat krapt de krullen van de trap</p>
       </div>
-      <footer className="absolute bottom-0 p-4 flex items-center justify-center w-screen bg-sky-600">
+      <footer className="fixed bottom-0 p-4 flex items-center justify-center w-screen bg-sky-600">
         <SpeechRecognitionComponent />
       </footer>
     </div>
