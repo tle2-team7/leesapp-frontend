7d4eab4cf553ab5e6f0b7d57ce87f5124cf91fb5
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-04-19 11:08:00 +0200

    removed unused folders added folders removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <div>
       <h1>lees app goes here...</h1>
     </div>
   );
