030f6696e509d6dfe6be608cf6cb96816633b471
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-04-25 12:04:11 +0200

    changed example text to read

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <h1 className="text-2xl">Speech recognition</h1>
       </header>
       <div id="speechRecognitionOutput" className="w-screen relative mb-24">
-        <p className="sentenceToRead">De kat krapt de krullen van de trap</p>
+        <p className="sentenceToRead">Hier komt tekst te staan wat moet worden voorgelezen. Deze komt vanuit de back-end en is gegenereerd door chatGPT</p>
       </div>
       <footer className="fixed bottom-0 p-4 flex items-center justify-center w-screen bg-sky-600">
         <SpeechRecognitionComponent />
