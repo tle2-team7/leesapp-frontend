1db07173f2dd7db78dc679f60cdd40288c1423ff
LuukFTF <lucasvdvegt@gmail.com>
2023-04-26 15:39:23 +0200

    add routes

---
+++ src/assignment_selection/AssignmentSelection.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function AssignmentSelection() {
+
+  return (
+    <div className="AssignmentSelection">
+        <h1>AssignmentSelection Screen goes here</h1>
+    </div>
+  );
+}
+
+export default AssignmentSelection;
