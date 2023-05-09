cc75aea82bfb7f8196d144899641b19a97448261
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-05-09 11:24:48 +0200

    styling

--- src/components/ToolTipComponent.tsx
+++ src/components/ToolTipComponent.tsx
@@ -4,5 +4,5 @@ type TToolTipProps = {
 };
 
 export default function ToolTipComponent(props: TToolTipProps) {
-  return <p className={`${props.visible ? "block" : "hidden"} absolute w-64 bottom-24 left-1/2 transform -translate-x-1/2 bg-white p-4 text-black text-xs rounded-lg text-center`}>{props.text}</p>;
+  return <p className={`${props.visible ? "block" : "hidden"} w-28 absolute bottom-24 left-1/2 transform -translate-x-1/2 bg-white p-2 text-black text-xs rounded-lg text-center`}>{props.text}</p>;
 }
