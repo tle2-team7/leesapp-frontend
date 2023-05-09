471e9649d10d4ed7629a86dd870c0b2d71d4149a
LucasvdVegt <lpsvandervegt@gmail.com>
2023-05-09 16:04:16 +0200

    tailwind stuff

--- src/assignment/Assignment.tsx
+++ src/assignment/Assignment.tsx
@@ -16,16 +16,16 @@ function Assignment() {
 
   return (
     <div className="Assignment">
-      <header className="p-4 items-center justify-center w-screen bg-sky-600">
+      <header className="p-4 items-center justify-center w-screen bg-secondary-main-500 ">
         <h1 className="text-2xl">Speech recognition</h1>
       </header>
       <div id="speechRecognitionOutput" className="w-screen relative mb-24">
-        <p className="sentenceToRead">Hier komt tekst te staan wat moet worden voorgelezen. Deze komt vanuit de back-end en is gegenereerd door chatGPT</p>
+        <p className="sentenceToRead bg-surface-main-150">Hier komt tekst te staan dat moet worden voorgelezen. Deze komt vanuit de back-end en is gegenereerd door chatGPT</p>
         {messages.map((message, idx) => (
           <MessageComponent key={idx} message={message} />
         ))}
       </div>
-      <footer className="fixed bottom-0 p-4 flex items-center justify-center w-screen bg-sky-600">
+      <footer className="fixed bottom-0 p-4 flex items-center justify-center w-screen bg-secondary-main-500">
         <SpeechRecognitionComponent createMessage={createMessage} />
       </footer>
     </div>
--- src/assignment_selection/AssignmentSelection.tsx
+++ src/assignment_selection/AssignmentSelection.tsx
@@ -4,7 +4,31 @@ function AssignmentSelection() {
 
   return (
     <div className="AssignmentSelection">
-        <h1>AssignmentSelection Screen goes here</h1>
+        <header>
+          <h1>AssignmentSelection Screen goes here</h1>
+        </header>
+        
+        <div className="bg-surface-main-150">
+          <div>
+            <p>Boodschappen </p>
+          </div>
+          <div>
+            <p>0/10</p>
+          </div>
+        </div>
+
+        <div className="bg-surface-main-150">
+          <div>
+            <p>Hobby</p>
+          </div>
+          <div>
+            <p>0/10</p>
+          </div>
+        </div>
+
+        <footer>
+          footer
+        </footer>
     </div>
   );
 }
