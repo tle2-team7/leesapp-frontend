4afde54864915859dba5a58dd82089368c2bfe68
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-05-16 12:55:29 +0200

    changed code structure, messages now call the api and loading icon is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import SpeechRecognitionComponent from "./components/SpeechRecognitionComponent";
 import MessageComponent from "./components/MessageComponent";
-import { postPrompt } from "./api/postPrompt";
 
 export type TMessage = {
-  text: string;
+  text: string; //user message
+  prompt: string; //prompt to make a gpt response
   isUser: boolean;
   isFirst: boolean;
 };
@@ -12,37 +12,32 @@ export type TMessage = {
 function App() {
   const [messages, setMessages] = useState<TMessage[]>([]);
 
-  function createMessage(text: string, isUser: boolean, isFirst: boolean) {
-    setMessages((prevMessages) => [...prevMessages, { text: text, isUser: isUser, isFirst: isFirst }]);
-  }
-
-  const promptTranscript = async (transcript: string) => {
-    const response = await postPrompt(transcript);
-    createMessage(response.choices[0].message.content, false, false);
-  };
-
   //shouldrun ensures that the useEffect only runs once, without this the useEffect runs twice
   //because of react strict mode being used in tandem with npm run dev
   const shouldRun = useRef(true);
   useEffect(() => {
     if (shouldRun.current) {
       shouldRun.current = false;
-      createMessage("", false, true);
+      createMessage("", "", false, true);
     }
   }, []);
+
+  function createMessage(text: string, prompt: string, isUser: boolean, isFirst: boolean) {
+    setMessages((prevMessages) => [...prevMessages, { text: text, prompt: prompt, isUser: isUser, isFirst: isFirst }]);
+  }
 
   return (
     <div className="App">
       <header className="p-4 items-center justify-center w-screen bg-sky-600">
-        <h1 className="text-2xl">Speech recognition</h1>
+        <h1 className="text-2xl text-center">Leer lezen met GPT</h1>
       </header>
       <div className="w-screen relative mb-24">
         {messages.map((message, idx) => (
-          <MessageComponent key={idx} message={message} />
+          <MessageComponent key={idx} text={message.text} prompt={message.prompt} isUser={message.isUser} isFirst={message.isFirst} />
         ))}
       </div>
       <footer className="fixed bottom-0 p-4 flex items-center justify-center w-screen bg-sky-600">
-        <SpeechRecognitionComponent createMessage={createMessage} promptTranscript={promptTranscript} />
+        <SpeechRecognitionComponent createMessage={createMessage} />
       </footer>
     </div>
   );
