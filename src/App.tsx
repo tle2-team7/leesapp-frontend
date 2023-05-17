1453964dabce702847064014bdeb5f25dba58985
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-05-17 14:09:11 +0200

    added autoscrolling to most recent message, but it is still a little buggy

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export type TMessage = {
 
 function App() {
   const [messages, setMessages] = useState<TMessage[]>([]);
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
 
   //shouldrun ensures that the useEffect only runs once, without this the useEffect runs twice
   //because of react strict mode being used in tandem with npm run dev
@@ -21,6 +22,19 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    scrollToBottom();
+    console.log(messages);
+  }, [messages]);
+
+  function scrollToBottom() {
+    if (messagesContainerRef.current) {
+      setTimeout(() => {
+        messagesContainerRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
+      }, 100);
+    }
+  }
+
   function createMessage(text: string, prompt: string, isUser: boolean) {
     setMessages((prevMessages) => [...prevMessages, { text: text, prompt: prompt, isUser: isUser }]);
   }
@@ -30,7 +44,7 @@ function App() {
       <header className="p-4 items-center justify-center w-screen bg-sky-600">
         <h1 className="text-2xl text-center">Leer lezen met GPT</h1>
       </header>
-      <div className="w-screen relative mb-24">
+      <div className="w-screen relative mb-24" ref={messagesContainerRef}>
         {messages.map((message, idx) => (
           <MessageComponent key={idx} text={message.text} prompt={message.prompt} isUser={message.isUser} />
         ))}
