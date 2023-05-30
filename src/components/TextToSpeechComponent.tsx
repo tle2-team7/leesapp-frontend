6b79b7e2308ebe053f92b381b89408424509acf9
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-05-30 13:35:01 +0200

    set language to dutch

--- src/components/TextToSpeechComponent.tsx
+++ src/components/TextToSpeechComponent.tsx
@@ -4,6 +4,7 @@ export default function speak(event: React.MouseEvent<HTMLDivElement>) {
   const sentence = (event.target as HTMLElement).textContent;
   if (sentence !== null) {
     const utterThis = new SpeechSynthesisUtterance(sentence);
+    utterThis.lang = "nl";
     synth.speak(utterThis);
   }
 }
